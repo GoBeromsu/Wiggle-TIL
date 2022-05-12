064303e373809ba89b77d0a195524aa9bf051890
goberomsu <310o@naver.com>
2022-05-12 15:53:41 +0900

    add : 알고리즘 문제 풀이

--- Algorithm/불안정지역.cpp
+++ Algorithm/불안정지역.cpp
@@ -12,16 +12,18 @@ public:
     int index;  // 도시의 인덱스
     int income; // 해당 도시의 소득
 
+    // 도시의 인덱스와 수입을 저장합니다.
     City(int index, int income)
     {
         this->index = index;
         this->income = income;
     }
-
+    // 오퍼레이션을 재정의하여 city의 소득을 비교하도록 합니다.
     bool operator<(const City &o) const
     {
         return this->income < o.income;
     }
+    // 오퍼레이션을 재정의하여 city의 소득을 비교하도록 합니다.
     bool operator>(const City &o) const
     {
         return this->income > o.income;
@@ -30,6 +32,8 @@ public:
 
 int getMaximumRangeDifference(int n, int k, const vector<City> &cities)
 {
+
+    // 최대 소득 차를 저자할 변수를 선언합니다.
     int answer = 0;
 
     // 소득이 가장 작은 도시부터 pop되는 우선순위 큐
@@ -37,20 +41,24 @@ int getMaximumRangeDifference(int n, int k, const vector<City> &cities)
 
     // 소득이 가장 높은 도시부터 pop되는 우선순위 큐
     priority_queue<City> rangeMaximum;
-
+    // max와 min 큐에 도시들을 저장합니다.
     for (int i = 0; i < k - 1; i++)
     {
         rangeMaximum.push(cities[i]);
         rangeMinimum.push(cities[i]);
     }
+
     for (int i = k - 1; i < n; i++)
     {
+        //우선 순위 큐에서 자동으로 도시 소득의 최대와 최솟값을 구한다.
         rangeMaximum.push(cities[i]);
         rangeMinimum.push(cities[i]);
+        //현재 탐색하는 부분의 인덱스가 아닌 경우 그 값을 제거합니다.
         while (rangeMaximum.top().index < i - k + 1)
             rangeMaximum.pop();
         while (rangeMinimum.top().index < i - k + 1)
             rangeMinimum.pop();
+        // 이전 소득 차와 계속 비교를 하여 최대 소득 차를 구한다.
         answer = max(answer, rangeMaximum.top().income - rangeMinimum.top().income);
     }
 
@@ -59,27 +67,32 @@ int getMaximumRangeDifference(int n, int k, const vector<City> &cities)
 
 void process(int caseIndex)
 {
+
+    // 도시의 수와 연속되는 도시의 수를 입력 받습니다.
     int n, k;
     cin >> n >> k;
-    vector<City> cities;
+    vector<City> cities; // 도시들의 정보를 저장할 벡터를 선언한다.
 
     for (int i = 0; i < n; i += 1)
     {
+        // 도시의 인덱스와 수입을 입력 받아 저장합니다.
         int income;
         cin >> income;
         cities.push_back(City(i, income));
     }
-
+    // 도시 간 소득의 최댓 값을 저장합니다.
     int answer = getMaximumRangeDifference(n, k, cities);
-
+    // 닶을 출력합니다.
     cout << answer << endl;
 }
 
 int main()
 {
+    // 테스트 케이스의 갯수를 입력 받습니다.
     int caseSize;
     cin >> caseSize;
 
+    // 순차적으로 각 케이스를 실행합니다.
     for (int caseIndex = 1; caseIndex <= caseSize; caseIndex += 1)
     {
         process(caseIndex);
--- Algorithm/조세푸스의문제.cpp
+++ Algorithm/조세푸스의문제.cpp
@@ -2,17 +2,18 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
+#pragma warning(disable : 4996)
 using namespace std;
 
-
-class Player {
+class Player
+{
 public:
-	int index;
+    int index;
 
-	Player(int index = 0) {
-		this->index = index;
-	}
+    Player(int index = 0)
+    {
+        this->index = index;
+    }
 };
 
 /**
@@ -23,68 +24,78 @@ public:
  * @param players   좌석에 앉아있는 순서대로 주어지는 플레이어 정보
  * @return
  */
-vector<Player> getDeadPlayersList(int n, int m, const vector<Player>& players) {
-	// 현재 게임에서 제외된 플레이어들의 리스트
-	vector<Player> deadPlayers;
+vector<Player> getDeadPlayersList(int n, int m, const vector<Player> &players)
+{
+    // 현재 게임에서 제외된 플레이어들의 리스트
+    vector<Player> deadPlayers;
 
-	// 아직 게임에서 제외되지 않는 플레이어들의 리스트
-	queue<Player> playerQueue;
+    // 아직 게임에서 제외되지 않는 플레이어들의 리스트
+    queue<Player> playerQueue;
+    // 풀래이어들을 순차적으로 입력한다.
+    for (int i = 0; i < n; i += 1)
+    {
+        playerQueue.push(players[i]);
+    }
 
-	for (int i = 0; i < n; i += 1) {
-		playerQueue.push(players[i]);
-	}
-
-	for (int i = 0; i < n; i++) {
-		// (m-1)명의 사람을 건너뛴다.
-		int jump = 1 + (m - 1) % playerQueue.size();
-		for (int j = 0; j < jump - 1; j += 1) {
-			Player p = playerQueue.front();
-			playerQueue.pop();
-			playerQueue.push(p);
-		}
-
-		// m번째 사람은 게임에서 제외한다.
-		Player dead = playerQueue.front();
-		playerQueue.pop();
-
-		// 제외 리스트에 추가한다.
-		deadPlayers.push_back(dead);
-	}
-
-
-	return deadPlayers;
+    // index는 제외할 사람의 순서이다. 즉 원형에서 m 번째 사람을 의미한다.
+    int index = m;
+    for (int i = 0; i < n; i++)
+    {
+        // 테이블이 원형이므로 m-1명을 건너뛰어야 m번째가 된다.
+        index = 1 + (m - 1) % playerQueue.size();
+        for (int j = 0; j < index - 1; j++)
+        {
+            // 순차적으로 m 번째까 될 때까지 테이블을 회전 시킨다.
+            Player p = playerQueue.front();
+            playerQueue.pop();
+            playerQueue.push(p);
+        }
+        // m 번째 사람은 게임에서 제외한다.
+        Player e = playerQueue.front();
+        playerQueue.pop();
+        // 제외 대상자를 리스트에 추가한다.
+        deadPlayers.push_back(e);
+    }
+    return deadPlayers;
 }
 
-void testcase(int caseIndex) {
-	int n, m;
-	scanf("%d %d", &n, &m);
+void testcase(int caseIindex)
+{
+    int n, m; // n과 m을 입력 받는다.
+    scanf("%d %d", &n, &m);
 
-	vector<Player> players;
-	for (int i = 0; i < n; i++) {
-		players.push_back(Player(i + 1));
-	}
+    vector<Player> players; //플레이어들을 저장할 player 벡터를 선언한다.
+    for (int i = 0; i < n; i++)
+    {
+        players.push_back(Player(i + 1)); // 1~n까지 player들의 인덱스를 지정한다.
+    }
+    // 제외되는 플레이어의 순서를 구한다.
+    vector<Player> deadPlayers = getDeadPlayersList(n, m, players);
 
-	vector<Player> deadPlayers = getDeadPlayersList(n, m, players);
-
-	for (int i = 0; i < n; i++) {
-		if (i > 0) {
-			printf(" ");
-		}
-
-		Player p = deadPlayers[i];
-		printf("%d", p.index);
-	}
-	printf("\n");
+    // 제외되는 순서에 따라 플레이어를 출력한다.
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 0)
+        {
+            printf(" ");
+        }
+        // 제외된 플레이러를 지정한다.
+        Player p = deadPlayers[i];
+        printf("%d", p.index);
+    }
+    // 다음 케이스를 위해 줄을 바꾼다
+    printf("\n");
 }
 
-int main() {
-	int caseSize;
-	scanf("%d", &caseSize);
+int main()
+{
+    int caseSize;           //테스트의 케이스의 갯수를 저장할 변수를 선언한다.
+    scanf("%d", &caseSize); // 테스트 케이스의 갯수를 저장한다.
+                            // 테스트 케이스를 실행한다.
+    for (int caseIndex = 1; caseIndex <= caseSize; caseIndex += 1)
+    {
+        testcase(caseIndex);
+    }
 
-	for (int caseIndex = 1; caseIndex <= caseSize; caseIndex += 1) {
-		testcase(caseIndex);
-	}
-
-	return 0;
+    return 0;
 }
-
