843ef146af4de9882c6c4174872e513d7d728e54
goberomsu <gobeumsu@gmail.com>
2023-07-17 11:27:50 +0900

    Fix : typo

--- Machine Learning/Linear Model.py
+++ Machine Learning/Linear Model.py
@@ -4,10 +4,9 @@ import matplotlib.pyplot as plt
 x_data = [1.0, 2.0, 3.0]
 y_data = [2.0, 4.0, 6.0]
 
-w = 1.0
 
 def forward(x):
-    return x * x
+    return x * w
 
 def loss(x, y):
     y_pred = forward(x)
@@ -22,7 +21,7 @@ for w in np.arange(0.0, 4.1, 0.1):
     for x_val, y_val in zip(x_data,y_data):
         y_pred_val = forward(x_val)
         l = loss(x_val,y_val)
-        l_sum += 1
+        l_sum += l
         print("\t", x_val, y_val, y_pred_val, 1)
     print("MSE = ",l_sum/3)
     w_list.append(w)
